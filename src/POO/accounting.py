ae1c2daf40ed9d77d7852e51887907ca5ac01688
Sergio Flores Ledezma (zerch) <zerchito@gmail.com>
2023-09-17 23:21:22 -0400

    feat (poo): Add new accounting project

---
+++ src/POO/accounting.py
@@ -0,0 +1,19 @@
+class Account():
+
+  def __init__(self, owner, balance=0):
+    self.owner = owner
+    self.balance = balance
+  
+  def deposit(self, dep_amt):
+    self.balance = self.balance + dep_amt
+    print(f'Added {dep_amt} to the balance')
+
+  def withdrawal(self, wd_amt):
+    if self.balance >= self.balance - wd_amt:
+      self.balance -= wd_amt
+      print("Withdrawal accepted")
+    else:
+      print('Sorry non enough funds!')
+    
+  def __str__(self):
+    return f'Owner: {self.owner} \n Balance: {self.balance}'
